1c6f7d0cbca7bd3dab9886e1f35393133b7dca5b
leechanyoung <yab0403@naver.com>
2025-08-26 01:09:33 +0900

    cors check

--- src/app.js
+++ src/app.js
@@ -23,10 +23,10 @@ app.use(cors({
   origin: 'https://theiauto-front.vercel.app',
   credentials: true
 }));
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   credentials: true
+// }));
 app.use(cookieParser());
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json({ limit: '50mb' }));
