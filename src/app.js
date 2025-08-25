bc522994a44f07369704c2f3149733f53043b3f7
leechanyoung <yab0403@naver.com>
2025-08-26 01:08:41 +0900

    test deploy

--- src/app.js
+++ src/app.js
@@ -23,13 +23,13 @@ app.use(cors({
   origin: 'https://theiauto-front.vercel.app',
   credentials: true
 }));
-// app.use(cors({
-//   origin: 'http://localhost:3000',
-//   credentials: true
-// }));
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
 app.use('/theiauto/server/api/general', [articleUserRouter, categoryUserRouter, advertisementUserRouter]);
 app.use('/theiauto/server/api', [userRouter, categoryRouter, articleRouter, advertisementRouter]);
 app.use(globalErrorHandler);
